ff2a4aea9d7782fab384de8786cda607b37fb449
cjkacz <cjkacz@gmail.com>
2017-12-22 22:30:17 -0500

    Octify

--- octos.mk
+++ octos.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+$(call inherit-product, vendor/octos/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aicp_bullhead
+PRODUCT_NAME := octos_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
@@ -19,7 +19,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
     BUILD_FINGERPRINT="google/bullhead/bullhead:8.0.0/OPR6.170623.023/4409485:user/release-keys" \
     PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR6.170623.023 4409485 release-keys"
-
-# AICP Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="doc HD (semdoc)"
